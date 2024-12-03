71c978da76396cbf2445449bb4893e1d0037717c
Soukaina Mourabit <soukainamourabit9@gmail.com>
2024-12-03 12:30:21 +0100

    defer question 5

--- src/Defer.php
+++ src/Defer.php
@@ -1,90 +1,58 @@
-<?php declare(strict_types=1);
+<?php
 
-class Callback {
+declare(strict_types=1);
+
+class Callback
+{
     public function __construct(
         private mixed $cb,
         private array $args = [],
-    ) {}
+    ) {
+    }
 
-    public function call(): void {
+    public function call(): void
+    {
         call_user_func_array($this->cb, $this->args);
     }
 }
 
-class Defer {
-    public function __construct() {}
-
-    public function __destruct() {}
-}
-?>
-
-<?php
-
-declare(strict_types=1);
-// implements Iterator
-class FibonacciSequence 
+function add($a, $b)
 {
-    private array $sequence;
-    private int $index = 0;
-
-    public function __construct(int $n)
-    {
-        $this->fibonacci($n);
-    }
-
-    // public static function first(int $n): self
-    // {
-    //     return;
-    // }
-
-    // public static function range(int $start, int $length = -1): self
-    // {
-    //     return;
-    // }
-
-    public function current(): mixed
+    return  $a + $b;
+}
+function sub($a, $b)
+{
+    echo $a - $b;
+}
+class Defer
+{
+    public string $callableVar;
+    public array $callableArray = [];
+    public function __construct()
     {
     }
+    public static function init(){
 
-    public function key(): mixed
+    }
+    public function defer(string $callable)
     {
+        $this->callableVar = $callable;
+        array_push($this->callableArray, $callable);
     }
 
-    public function next(): void
+    public function __destruct()
     {
-    }
-
-    public function rewind(): void
-    {
-    }
-
-    public function valid(): bool
-    {
-        return true;
-    }
-
-    public function fibonacci(int $n): int
-    {
-        if ($n == 0) {
-            
-            return 0;
+        print_r($this->callableArray);
+        for ($i = count($this->callableArray)-1; $i >= 0; $i--) {
+          echo  call_user_func_array($this->callableArray[$i], [6,2]);
         }
-        if ($n == 1) {
-            return 1;
-        }
-
-        $previous1 = 0;
-        $previous2 = 1;
-        $current = 0;
-
-        for ($i = 2; $i <= $n; $i++) {
-            $current = $previous1 + $previous2;
-            $previous1 = $previous2;
-            $previous2 = $current;
-        }
-        return $current;
     }
 }
 
-$obj= new FibonacciSequence(7);
-echo $obj->fibonacci(7);
+function tryDefer()
+{
+    $obj = new Defer();
+    $obj->defer('add');
+    $obj->defer('sub');
+}
+tryDefer();
